0e6f2a80097df81f9e8d33ba27ae683b71c7a53e
Blacksun1234 <ecousin12@gmail.com>
2023-11-28 14:32:57 +0100

    Emissive materials

--- RayTracingSeries/src/WalnutApp.cpp
+++ RayTracingSeries/src/WalnutApp.cpp
@@ -22,6 +22,12 @@ public:
 		blueSphere.Albedo = { 0.2f, 0.3f, 1.0f };
 		blueSphere.Roughness = 0.1f;
 
+		Material& orangeSphere = m_Scene.Materials.emplace_back();
+		orangeSphere.Albedo = { 0.8f, 0.5f, 0.2f };
+		orangeSphere.Roughness = 0.1f;
+		orangeSphere.EmissionColor = orangeSphere.Albedo;
+		orangeSphere.EmissionPower = 2.0f;
+
 		{
 			Sphere sphere;
 			sphere.Position = glm::vec3{ 0.0f, 0.0f, 0.0f };
@@ -37,10 +43,19 @@ public:
 			sphere.MaterialIndex = 1;
 			m_Scene.Spheres.push_back(sphere);
 		}
+
+		{
+			Sphere sphere;
+			sphere.Position = glm::vec3{ 2.0f, 0.0f, 0.0f };
+			sphere.Radius = 1.0f;
+			sphere.MaterialIndex = 2;
+			m_Scene.Spheres.push_back(sphere);
+		}
 	}
 
 	virtual void OnUpdate(float ts) override {
-		m_Camera.OnUpdate(ts);
+		if (m_Camera.OnUpdate(ts))
+			m_Renderer.RestetFrameIndex();
 	}
 
 	virtual void OnUIRender() override
@@ -50,6 +65,12 @@ public:
 		if (ImGui::Button("Render")) {
 			Render();
 		}
+		
+		ImGui::Checkbox("Accumulate", &m_Renderer.GetSettings().Accumulate);
+
+		if (ImGui::Button("Reset"))
+			m_Renderer.RestetFrameIndex();
+
 		ImGui::End();
 
 		ImGui::Begin("Scene");
@@ -72,6 +93,8 @@ public:
 			ImGui::ColorEdit3("Albedo", glm::value_ptr(material.Albedo));
 			ImGui::DragFloat("Roughness", &material.Roughness, 0.05f, 0.0f, 1.0f);
 			ImGui::DragFloat("Metallic", &material.Metallic, 0.05f, 0.0f, 1.0f);
+			ImGui::ColorEdit3("Emission Color", glm::value_ptr(material.EmissionColor));
+			ImGui::DragFloat("Emission Power", &material.EmissionPower, 0.05f, 0.0f, FLT_MAX);
 			
 			ImGui::Separator();
 
